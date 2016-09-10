6f7a177cd0914fd6c5942630dc0927381bc5b18d
jbt <jbt_5@yahoo.com>
2016-09-10 12:22:59 -0700

    basic working

--- handler.js
+++ handler.js
@@ -5,15 +5,57 @@ const bot = require('bot');
 // const aws = require('aws-sdk');
 
 module.exports.input = (event, context, callback) => {
-    let responseBody = {
-    	status: 'ok',
-    	detail: 'example',
-    	responses: [ { primaryText: 'This is the primary text.'} ],
-    	context: event.body.context
-    }
-	bot.postToUrl(event.body.context.callbackUrl, responseBody, (result) => {
+    
+    let payload = getPayload(event);
+
+    console.log(payload);
+
+	bot.postToUrl(event.body.context.callbackUrl, payload, (result) => {
 		callback(null, { function: 'input', result: result });	
 	});
+};
+
+
+let getPayload = (event) => {
+
+	return {
+    	status: 'ok',
+    	detail: event.body.input,
+    	text: 'This is the primary text replay or the lead in text for list of items',
+    	items: [
+    		{ 
+    			color: '#00FF00',
+    			basic_text: 'This text is for bare-bones clients',
+    			title_text: 'Shown in the title area',
+    			subtitle_text: 'Shown in the subtitle area',
+    			text_fields: [
+	    			{
+						title: 'Slack Command',
+	    				text: event.body.input.action
+	    			},
+	    			{
+	    				title: 'Command Text',
+	    				text: event.body.input.text
+	    			}	
+    			],
+    			actions: [
+    				{
+    					type: 'button',
+    					text: 'Button text',
+    					payload: { fake: 'payload' },
+    					default: true
+    				},
+    				{
+    					type: 'button',
+    					text: 'Another button',
+    					payload: { fake: 'payload' }
+    				}
+    			]
+    		} 
+    	],
+    	context: event.body.context
+    };
+
 };
 
 // let callLambda = (functionName, event) => {
