a2e01d901760ad191e1836708cde965bc7624b4b
jbt <jbt_5@yahoo.com>
2016-09-10 17:33:05 -0700

    response updates

--- handler.js
+++ handler.js
@@ -10,7 +10,7 @@ module.exports.input = (event, context, callback) => {
 
     console.log(payload);
 
-	bot.postToUrl(event.body.context.callbackUrl, payload, (result) => {
+	bot.postToUrl(event.body.context.callback, payload, (result) => {
 		callback(null, { function: 'input', result: result });	
 	});
 };
@@ -21,13 +21,15 @@ let getPayload = (event) => {
 	return {
     	status: 'ok',
     	detail: event.body.input,
-    	text: 'This is the primary text replay or the lead in text for list of items',
+    	text: 'This is the primary text reply or the lead in text for list of items',
     	items: [
     		{ 
     			color: '#00FF00',
-    			basic_text: 'This text is for bare-bones clients',
+    			basic_text: 'This item text is for bare-bones clients',
     			title_text: 'Shown in the title area',
     			subtitle_text: 'Shown in the subtitle area',
+                footer_text: 'This is the footer',
+                image: 'https://farm9.staticflickr.com/8171/8028156532_4717b2be77_s.jpg',
     			text_fields: [
 	    			{
 						title: 'Slack Command',
@@ -41,17 +43,33 @@ let getPayload = (event) => {
     			actions: [
     				{
     					type: 'button',
+                        id: 'action_1',
     					text: 'Button text',
     					payload: { fake: 'payload' },
     					default: true
     				},
     				{
     					type: 'button',
+                        id: 'action_2',
     					text: 'Another button',
     					payload: { fake: 'payload' }
     				}
     			]
-    		} 
+    		},
+            { 
+                color: '#0000FF',
+                basic_text: 'This item text is for bare-bones clients',
+                title_text: 'Shown in the title area',
+                subtitle_text: 'Shown in the subtitle area',
+                footer_text: 'This is the footer',
+                image: 'https://img1.etsystatic.com/053/0/8863163/il_75x75.688855889_pehc.jpg',
+                text_fields: [
+                    {
+                        title: 'Sample',
+                        text: 'Just a sample field'
+                    }   
+                ]
+            }  
     	],
     	context: event.body.context
     };
